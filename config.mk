89c3ecbf42f1ece5333b06e4ff03615e3c64649f
Sanju0910 <sreeshankar0910@gmail.com>
2023-05-29 16:35:20 +0530

    apps: Reorganize the files * Migrate apps from system_ext to product * Nuke unwanted apps & frameworks * Include oplus frameworks * Rewrite permissions

--- config.mk
+++ config.mk
@@ -6,23 +6,18 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_PACKAGES += \
     OnePlusCamera \
     OnePlusCameraService \
-    OplusAppPlatform \
-    OplusExSystemService \
     OnePlusGallery
     
 PRODUCT_COPY_FILES += \
-    vendor/oneplus/apps/system/etc/permissions/com.oneplus.camera.service.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.oneplus.camera.service.xml \
-    vendor/oneplus/apps/system/etc/sysconfig/hiddenapi-package-oplus-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hiddenapi-package-oplus-whitelist.xml \
-    vendor/oneplus/apps/system_ext/etc/default-permissions/oneplus_camera_default_grant_permissions_list.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/default-permissions/oneplus_camera_default_grant_permissions_list.xml \
-    vendor/oneplus/apps/system_ext/etc/permissions/oplus_google_lens_config.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/oplus_google_lens_config.xml \
-    vendor/oneplus/apps/system_ext/etc/permissions/privapp-permission-oplus.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permission-oplus.xml \
-    vendor/oneplus/apps/system_ext/priv-app/OnePlusCamera/lib/arm64/libsnpe_dsp_v66_domains_v2_skel.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/priv-app/OnePlusCamera/lib/arm64/libsnpe_dsp_v66_domains_v2_skel.so
+    vendor/oneplus/apps/proprietary/system/etc/sysconfig/hiddenapi-whitelist-oplus.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hiddenapi-whitelist-oplus.xml \
+    vendor/oneplus/apps/proprietary/product/etc/permissions/com.oneplus.camera.service.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.oneplus.camera.service.xml \
+    vendor/oneplus/apps/proprietary/product/etc/permissions/com.oneplus.camera.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.oneplus.camera.xml \
+    vendor/oneplus/apps/proprietary/product/etc/permissions/com.oneplus.gallery.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.oneplus.gallery.xml \
+    vendor/oneplus/apps/proprietary/product/priv-app/OnePlusCamera/lib/arm64/libsnpe_dsp_v66_domains_v2_skel.so:$(TARGET_COPY_OUT_PRODUCT)/priv-app/OnePlusCamera/lib/arm64/libsnpe_dsp_v66_domains_v2_skel.so
     
 # Frameworks    
 PRODUCT_PACKAGES += \
-    oplus-support-wrapper \
-    com.oplus.camera.unit.sdk.adapter \
-    com.oplus.camera.unit.sdk \
+    oplus-framework
     
 PRODUCT_BOOT_JARS += \
-    oplus-support-wrapper
+    oplus-framework
