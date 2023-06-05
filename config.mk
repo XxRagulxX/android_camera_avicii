348715d490ecc10c7957fea3893a902b31899157
Sanju0910 <sreeshankar0910@gmail.com>
2023-06-05 13:47:19 +0000

    apps: Migrate apps from product partition to system_ext partition Reason: Product partition have limitations in intracting with vendor partition

--- config.mk
+++ config.mk
@@ -10,11 +10,12 @@ PRODUCT_PACKAGES += \
     
 PRODUCT_COPY_FILES += \
     vendor/oneplus/apps/proprietary/system/etc/sysconfig/hiddenapi-whitelist-oplus.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hiddenapi-whitelist-oplus.xml \
-    vendor/oneplus/apps/proprietary/product/etc/permissions/com.oneplus.camera.service.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.oneplus.camera.service.xml \
-    vendor/oneplus/apps/proprietary/product/etc/permissions/com.oneplus.camera.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.oneplus.camera.xml \
-    vendor/oneplus/apps/proprietary/product/etc/permissions/com.oneplus.gallery.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.oneplus.gallery.xml \
-    vendor/oneplus/apps/proprietary/product/priv-app/OnePlusCamera/lib/arm64/libsnpe_dsp_v66_domains_v2_skel.so:$(TARGET_COPY_OUT_PRODUCT)/priv-app/OnePlusCamera/lib/arm64/libsnpe_dsp_v66_domains_v2_skel.so \
-    vendor/oneplus/apps/proprietary/product/priv-app/OnePlusCameraService/lib/arm64/libCameraMDMHelper.so:$(TARGET_COPY_OUT_PRODUCT)/priv-app/OnePlusCameraService/lib/arm64/libCameraMDMHelper.so    
+    vendor/oneplus/apps/proprietary/system/etc/permissions/com.oneplus.camera.service.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.oneplus.camera.service.xml \
+    vendor/oneplus/apps/proprietary/system_ext/etc/permissions/com.oneplus.camera.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/com.oneplus.camera.xml \
+    vendor/oneplus/apps/proprietary/system_ext/etc/permissions/com.oneplus.gallery.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/com.oneplus.gallery.xml \
+    vendor/oneplus/apps/proprietary/system_ext/priv-app/OnePlusCamera/lib/arm64/libsnpe_dsp_v66_domains_v2_skel.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/priv-app/OnePlusCamera/lib/arm64/libsnpe_dsp_v66_domains_v2_skel.so \
+    vendor/oneplus/apps/proprietary/system/priv-app/OnePlusCameraService/lib/arm64/libCameraMDMHelper.so:$(TARGET_COPY_OUT_SYSTEM)/priv-app/OnePlusCameraService/lib/arm64/libCameraMDMHelper.so    
+
 # Frameworks    
 PRODUCT_PACKAGES += \
     oplus-framework
