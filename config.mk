2bc6c6186721ddc79629e2ff5f407ebc2c3965e1
sreeshankark <sreeshankar0910@gmail.com>
2024-07-05 10:14:54 +0530

    apps: Update apps for Android 14

--- config.mk
+++ config.mk
@@ -15,10 +15,3 @@ PRODUCT_COPY_FILES += \
     vendor/oneplus/apps/proprietary/system_ext/etc/permissions/com.oneplus.gallery.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/com.oneplus.gallery.xml \
     vendor/oneplus/apps/proprietary/system_ext/priv-app/OnePlusCamera/lib/arm64/libsnpe_dsp_v66_domains_v2_skel.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/priv-app/OnePlusCamera/lib/arm64/libsnpe_dsp_v66_domains_v2_skel.so \
     vendor/oneplus/apps/proprietary/system/priv-app/OnePlusCameraService/lib/arm64/libCameraMDMHelper.so:$(TARGET_COPY_OUT_SYSTEM)/priv-app/OnePlusCameraService/lib/arm64/libCameraMDMHelper.so    
-
-# Frameworks    
-PRODUCT_PACKAGES += \
-    oplus-framework
-    
-PRODUCT_BOOT_JARS += \
-    oplus-framework
